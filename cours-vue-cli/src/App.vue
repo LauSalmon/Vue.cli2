baf55abada755c6784924b8e03e490ae64adc26c
LauSalmon <salmon.laura@laposte.net>
2023-12-13 14:02:12 +0100

    ajout fichier vue.cli

---
+++ cours-vue-cli/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <div class="container"> -->
+    <h1 class="text-center">Les Amis</h1>
+    <ul class="list-group">
+      <!--<un-ami leNom="Steven Seagal" lePhone="0145965458" leMail="steven@seagal.fr" premium ></un-ami>
+      <un-ami leNom="Laura Salmon" lePhone="0619413177" leMail="salmon.laura@laposte.net" premium ></un-ami>
+      <un-ami leNom="Pomme" lePhone="N'en a pas" leMail="pomme@quessadilla.net" premium ></un-ami>
+      <un-ami  leNom="BadBoyHalo" lePhone="0139352825" leMail="blalba@globiboulga.net" premium></un-ami>   
+      -->
+      <un-ami v-for="unAmi in lesAmis" :key="unAmi.id" :leNom="unAmi.name" :lePhone="unAmi.phone" :leMail="unAmi.email" :premium="unAmi.premium" @mon-event-premium="afficherStatusPremium"></un-ami>
+   
+    </ul>
+  <!-- </div> -->
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      lesAmis: [
+        {
+            id: 'lasticot',
+            name: 'COCO L ASTICOT',
+            phone: '01234 5678 991',
+            email: 'coco@lasticot.com',
+            premium : true,
+        },
+        {
+            id: 'janine',
+            name: 'Janine DeLavega',
+            phone: '09876 543 221',
+            email: 'janine@delavega.com',
+            premium : false,
+        },
+    ],
+    }
+  },
+  methods: {
+    afficherStatusPremium() {
+      console.log("test, test, 1,2,1,2");
+    }
+  }
+}
+</script>
---
+++ cours-vue-cli/src/components/UnAmi.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container my-1">
+        <ul class="list-group">
+            <h2 class="list-group-item" >{{leNom}} - {{premiumData ?'Ami Premium' : 'Ami Nul' }}</h2> 
+            <button @click="afficherPremium" class="btn btn-danger">Premium ?</button>
+            <button @click="afficherDetails" class="btn btn-primary">Voir Détails</button>           
+            <ul v-if="detailsVisibles" class="list-group">
+                <li class="list-group-item">{{lePhone}}</li>
+                <li class="list-group-item">{{leMail}}</li>
+            </ul>
+        </ul>
+    </div> 
+</template>
+
+<script>
+export default {
+    // props: [
+    //     "leNom", 
+    //     "lePhone", 
+    //     "leMail",
+    //     "premium"
+    // ],
+    data(){
+        return{
+            detailsVisibles:false,
+            premiumData: this.premium,
+        }
+    },
+    props : {
+        leNom : {
+            type : String,
+            required : true,
+        },
+        lePhone : {
+            type : String,
+            required : true,
+        },
+        leMail : {
+            type : String,
+            required : true,
+        },
+        premium : {
+            type : Boolean,
+            required : false,
+            default : false,
+            // validator : function(value){
+            //     return value === '1' || value === '0';
+            // }
+        },
+                
+    },
+    methods:{
+        afficherDetails(){
+            this.detailsVisibles = !this.detailsVisibles;
+        },
+        afficherPremium() {
+            this.$emit("mon-event-premium")
+        //  this.premiumData = !this.premiumData;
+        //     if(this.premiumData === "1") {
+        //         this.premiumData = "0";
+        //     } else {
+        //         this.premiumData = "1";
+        //     }
+        // }
+    },
+}
+};
+</script>
